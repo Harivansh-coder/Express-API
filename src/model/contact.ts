65cb715f2439035d3996fa917e484df7341037da
adnan007d <adnanmansuri54@gmail.com>
2023-03-04 13:24:17 +0530

    removed date field and added timestamps

--- src/model/contact.ts
+++ src/model/contact.ts
@@ -1,23 +1,30 @@
 import mongoose from "mongoose";
 
-const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    // No Need for this
+    // date: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  // Adds createdAt and updatedAt fields
+  {
+    timestamps: true,
+  }
+);
 
 const contactModel = mongoose.model("Contact", contactSchema);
 
